27d576a338e0523a02ca2d8657019783f11c2408
phedias <daniel.borgs@noxa.de>
2025-06-03 14:20:47 +0200

    docs: add additional funcs for reference

--- L5/name_change4.asm
+++ L5/name_change4.asm
@@ -2,35 +2,52 @@ section .data
 	name dd 'Pete'
 	name_length equ $ - name
 	nl db 0xa,0
-	fmt db 10,'Der Byte-Wert in der Speicherzelle = %c', 10, 10, 0 ; ugh ._. 10 => \n in ASCII so '\nDer Byte...\n\n' with null-terminated
+	fmt db 'Der Byte-Wert in der Speicherzelle = %c', 10, 10, 0 ; ugh ._. 10 => \n in ASCII so '\nDer Byte...\n\n' with null-terminated
 
 section .text
 global main
 extern printf
 
 main:
-	mov ecx, name
-	call print_with_nl
-	mov ecx, name + 1
-	call print_with_nl
-	mov ecx, name + 2
-	call print_with_nl
-	mov ecx, name + 3
-	call print_with_nl
-
 	xor esi, esi ; init loop at 0 --> faster than zeroing esi any other way (most of the time)
+	call char_loop
+
+	xor esi, esi
 	call printf_loop
 
 	mov eax, 1
 	mov ebx, 0
 	int 80h
 
-print_with_nl:
+char_loop:
+	mov ecx, name
+	; works because we increment the pointer
+	add ecx, esi
 	call print_char
 	; write expects an address in ecx and not the val... so writing `mov ecx, 0xa` does not work
 	mov ecx, nl
 	call print_char
+
+	inc esi
+	cmp esi, name_length
+	jl char_loop
 	ret
+
+print_with_nl_padding:
+	; Get the byte to print
+	movzx eax, byte[ecx]  ; Zero-extend byte to eax
+	push 0x0a00            ; Push newline + padding
+	mov [esp+1], al        ; Overwrite padding with character
+
+	; Print both bytes at once
+	mov ecx, esp
+
+print_with_nl_shift:
+	movzx eax, byte[ecx]  ; Get character
+	shl eax, 8             ; Shift left by 8 bits
+	or eax, 0x0a           ; Add newline
+	push eax               ; Push to stack
+	mov ecx, esp
 
 print_char:
 	mov eax, 4
