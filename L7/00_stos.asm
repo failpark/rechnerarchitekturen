ba3b57e45d6e12c1e3d330fb7d82e205663780d7
phedias <daniel.borgs@noxa.de>
2025-06-11 17:31:06 +0200

    refactor: stosb

--- L7/00_stos.asm
+++ L7/00_stos.asm
@@ -1,16 +1,25 @@
 section .data
 	; can't NULL-Terminate here since we would process garbage (0x20 is space so it's fine here but still...)
 	; or handle it seperatly
-	s1 db 'HELLO, WORLD', 0
-	len equ $ - s1 - 1
+	s1 db 'HELLO, WORLD', 10, 0
+	len equ $ - s1
 
 section .bss
-	s2 resb len + 1
+	s2 resb len
 
 section .text
 global main
+
+print_ecx:
+	mov eax, 4
+	mov ebx, 1
+	mov edx, len
+	int 80h
+	ret
+
 main:
-	mov ecx, len
+	; we don't want to touch the \n and NULL at the end
+	mov ecx, len - 2
 	mov esi, s1
 	mov edi, s2
 	cld
@@ -20,11 +29,16 @@ main:
 		stosb
 	loop loop_here
 
-	mov eax, 4
-	mov ebx, 1
+	; add \n to s2
+	mov byte[edi], 0xa
+	inc edi
+	mov byte[edi], 0x0
+
+	mov ecx, s1
+	call print_ecx
+
 	mov ecx, s2
-	mov edx, len
-	int 80h
+	call print_ecx
 	
 	mov eax, 1
 	xor ebx, ebx
